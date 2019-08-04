43adbba962f460ba826a4e9856c7be9f2f6b5791
JustynaKop <justyna.kopiec@o2.pl>
2019-08-04 10:46:53 +0200

    Modyfikacja plików

--- js/main.js
+++ js/main.js
@@ -32,6 +32,17 @@ $(document).ready(function(){
                 scrollTop: $(".aboutme").offset().top
             }, 1000)
         }) 
+		$('a[href^="#"]').on('click', function(event) {
+	
+		var target = $( $(this).attr('href') );
+	
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 1000);
+		}
+	});
 });
 
 function initializeMenuState(){
